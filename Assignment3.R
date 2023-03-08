4b1a75d2f651c9e06b8af189c64bc78bee68e26b
keshavraghavan <keshav.raghavan@utdallas.edu>
2023-03-07 20:14:10 -0600

    Commit at 2015

--- Assignment3.R
+++ Assignment3.R
@@ -1,1 +1,16 @@
+#install.packages("rpart")
+library("rpart")
+
+
 heart <- data.frame(read.csv("heart.csv"))
+
+data <- na.omit(heart)
+i <- duplicated(data)
+
+clean.heart <- unique(heart[complete.cases(heart),])
+heart.refined <- clean.heart[clean.heart$Cholesterol > 40,]
+
+heart_classification_tree <- rpart(HeartDisease ~ FastingBS + Age + Sex,
+                                   data = heart.refined,
+                                   method = "class")
+heart_classification_tree
